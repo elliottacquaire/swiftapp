110d8f47ea3c29e199fb8281696b5f78852df7ca
xuyang.li <xuyangl@wicresoft.com>
2020-12-21 14:55:34 +0800

    Initial Commit

---
+++ PorchTestProgram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PorchTestProgram
+//
+//  Created by Elliott on 2020/12/21.
+//  Copyright © 2020 ElliottTest. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
